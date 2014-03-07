a49cdf8fa6dd4bf6a30723c4bf4d7267ba89faa6
Chip Castle <chip@chipcastle.com>
2014-03-07 17:17:28 -0600

    Remove port config

--- config.lua
+++ config.lua
@@ -4,10 +4,8 @@ do
   config = _obj_0.config
 end
 config("production", function()
-  port("80")
   return code_cache("on")
 end)
 return config("development", function()
-  port("8080")
   return code_cache("off")
 end)
--- config.moon
+++ config.moon
@@ -1,9 +1,7 @@
 import config from require "lapis.config"
 
 config "production", ->
-  port "80"
   code_cache "on"
 
 config "development", ->
-  port "8080"
   code_cache "off"
